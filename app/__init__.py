d5075ede524a7bfbb75e0a755e6e19b5e903a332
Andreiuk Dmytro <andreiuk.dmytro@gmail.com>
2022-11-12 11:52:05 +0100

    urls change

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ def main():
 
     with RequestsSession() as session:
         url_arr=[
-            'https://www.subito.it/annunci-lazio/vendita/auto/latina/?ps=2000&pe=8000&ms=5&me=22&fu=2&gr=1&dr=2',
+            'https://www.subito.it/annunci-lazio/vendita/auto/latina/?ps=1000&pe=8000&ms=5&me=22&fu=2&gr=1',
             'https://www.subito.it/annunci-lazio/vendita/caravan-e-camper/roma/?ps=1000&pe=3000&crt=2',
             'https://www.subito.it/annunci-lazio/vendita/caravan-e-camper/latina/?ps=1000&pe=3000&crt=2',
             'https://www.subito.it/annunci-campania/vendita/caravan-e-camper/napoli/?ps=1000&pe=3000&crt=2'
